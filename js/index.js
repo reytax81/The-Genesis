912f4372c29a9a380c2886ad9b09f206766a31fa
boucaret yann <yann.boucaret@gmail.com>
2023-07-13 22:01:06 +0200

    commit5

--- js/index.js
+++ js/index.js
@@ -23,4 +23,41 @@ function updateDateTime() {
 // Met à jour la date et l'heure toutes les secondes
 setInterval(updateDateTime, 1000);
 
+// Récupère le formulaire de contact par son ID
+var contactForm = document.getElementById('contact-form');
+
+// Écoute l'événement "submit" du formulaire
+contactForm.addEventListener('submit', function(event) {
+    // Empêche l'envoi du formulaire par défaut
+    event.preventDefault();
+
+    // Récupère les valeurs des champs du formulaire
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Vérifie si les champs sont vides
+    if (name === '' || email === '' || message === '') {
+        alert('Veuillez remplir tous les champs du formulaire.');
+        return;
+    }
+
+    // Envoie les données du formulaire vers le serveur (vous devez implémenter cette partie côté serveur)
+    // Exemple :
+    // fetch('/contact', {
+    //     method: 'POST',
+    //     body: JSON.stringify({ name: name, email: email, message: message })
+    // })
+    // .then(function(response) {
+    //     if (response.ok) {
+    //         alert('Votre message a été envoyé avec succès.');
+    //         contactForm.reset();
+    //     } else {
+    //         alert('Une erreur s\'est produite lors de l\'envoi du message. Veuillez réessayer.');
+    //     }
+    // })
+    // .catch(function(error) {
+    //     console.error('Erreur :', error);
+    // });
+});
 
---
+++ send-email.php
@@ -0,0 +1,31 @@
+<?php
+// Récupère les données du formulaire
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Adresse e-mail de destination
+$to = 'yann.radureau@gmail.com';
+
+// Sujet de l'e-mail
+$subject = 'Nouveau message du formulaire de contact';
+
+// Corps de l'e-mail
+$body = "Nom : $name\n\n";
+$body .= "Adresse e-mail : $email\n\n";
+$body .= "Message :\n$message\n";
+
+// En-têtes de l'e-mail
+$headers = "From: $name <$email>\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+// Envoi de l'e-mail
+$mailSent = mail($to, $subject, $body, $headers);
+
+// Vérifie si l'e-mail a été envoyé avec succès
+if ($mailSent) {
+    echo 'success';
+} else {
+    echo 'error';
+}
+?>
