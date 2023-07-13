0bd4be8a4512d0c5641addea05827928d4e08fd0
boucaret yann <yann.boucaret@gmail.com>
2023-07-13 19:50:49 +0200

    commit4

--- js/index.js
+++ js/index.js
@@ -10,3 +10,17 @@ var button = document.getElementById('myButton');
 
 // Ajoute un écouteur d'événement pour déclencher la fonction showAlert lorsque le bouton est cliqué
 button.addEventListener('click', showAlert);
+
+// Récupère l'élément avec l'ID "date-time"
+var dateTimeElement = document.getElementById('date-time');
+
+// Fonction qui met à jour l'élément avec la date et l'heure actuelles
+function updateDateTime() {
+    var currentDate = new Date();
+    dateTimeElement.innerHTML = currentDate.toLocaleString();
+}
+
+// Met à jour la date et l'heure toutes les secondes
+setInterval(updateDateTime, 1000);
+
+
